953a9704ff0f2e9edcb313ff46b83513198f0d3d
miller9 <millerpoli9@gmail.com>
2020-04-16 15:55:17 -0400

    eliminando ejecucion de consulta, la tabla -tasks- ya fue creada

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const sqlite3 = require('sqlite3');
 // El arg que se envia es el nombre que se le va a dar a la BDD (proyecto-backend-db)
 let db = new sqlite3.Database('proyecto-backend-db');
 
-// ejecutar consulta para crear tabla "tasks"
-db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))');
+// ejecutar consulta para crear tabla "tasks", luego del commit comentar, para no ejecutar otra vez
+// db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))');
+
+
 
 // cerrar la conexion
 db.close();
