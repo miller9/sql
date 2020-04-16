406d59f16dc87094ce4147c148b4e56afe162ac9
miller9 <millerpoli9@gmail.com>
2020-04-16 15:48:08 -0400

    creando BDD=proyecto-backend-db y tabla=tasks

---
+++ server.js
@@ -0,0 +1,13 @@
+
+// importar libreria sqlite3
+const sqlite3 = require('sqlite3');
+
+// crear objeto para manipular BDD y a la vez, abrir conexion.
+// El arg que se envia es el nombre que se le va a dar a la BDD (proyecto-backend-db)
+let db = new sqlite3.Database('proyecto-backend-db');
+
+// ejecutar consulta para crear tabla "tasks"
+db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))');
+
+// cerrar la conexion
+db.close();
